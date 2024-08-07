6e98d7055cca5c03a7260f516c164a87f4b28b6c
Jorge Cutruzzola <jorgecutruzzola@gmail.com>
2024-08-07 02:53:07 -0300

    2da pre entrega

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -15,14 +15,20 @@ const productsRender = products => {
 
     products.forEach(item => {
         const card = document.createElement("div");
-        card.innerHTML = `
-                            <p> ${item.id} </p>
-                            <p> ${item.title} </p>
-                            <p> ${item.description} </p>
-                            <p> ${item.price} </p>
-                            <button> Eliminar </button>
-                        
-                        `
+        card.classList = "container-fluid"
+        card.innerHTML =
+                         `
+                            <div class="card" style="width: 18rem;">
+                                <img src="..." class="card-img-top" alt="...">
+                                <div class="card-body">
+                                    <h5 class="card-title">${item.id} - ${item.title}</h5>
+                                    <p class="card-text"> ${item.description}</p>
+                                    <p class="card-text"> ${item.price}</p>
+                                    <button class="btn btn-primary"> Eliminar </button>
+                                </div>
+                             </div>
+                          `
+
 
     
         productsContainer.appendChild(card);
@@ -45,3 +51,10 @@ const deleteProduct = (id) => {
     socket.emit("deleteProduct", id);
 
 }
+
+
+/*   <p> ${item.id} </p>
+                            <p> ${item.title} </p>
+                            <p> ${item.description} </p>
+                            <p> ${item.price} </p>
+                            <button> Eliminar </button> */
